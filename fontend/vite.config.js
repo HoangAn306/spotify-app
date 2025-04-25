003b5fc7ed73785de178342c8e072e0a0899b27b
Hoàng An <an3dhome@gmail.com>
2025-04-25 10:48:24 +0700

    Them trang admin

--- fontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/', // <- Rất quan trọng! Gốc của hosting
-  plugins: [react()],
-})
